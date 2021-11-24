3bdbb75d6739c6a16cba0e8470eba5e7d5f86c94
up201704219 <up201704219@fe.up.pt>
2021-11-24 15:28:34 +0000

    Added basis for receiving and sending data to the robot

--- assignement_b/src/reactive_turtlebot.cpp
+++ assignement_b/src/reactive_turtlebot.cpp
@@ -1,41 +1,45 @@
-// This program pub l ishes randomly−generated v e l o c i t y
-// messages for tu r t les im .
-
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
-#include <geometry_msgs/Twist.h> // For geometry_msgs : : Twist
+#include <geometry_msgs/Twist.h>
 #include <stdlib.h>
 #include <sstream>
 #include <bits/stdc++.h>
 #include <math.h>
+#include <vector>
+#include <string>
+
+using sensor_msgs::LaserScan;
+using geometry_msgs::Twist;
+using std::vector;
+using std::string;
+
+ros::Publisher pub;
+
+void laserCallback(const LaserScan::ConstPtr& scan) {
+
+  // Parsing scan data.
+  ROS_INFO("LaserScan (val,angle)=(%f,%f", msg->range_min,msg->angle_min);
+
+  // Creating Twist object.
+  Twist msg;
+
+  // Building msg.
+
+  // Publishing next movement data
+  //pub.publish(msg);
+}
 
 int main (int argc , char **argv) {
-  // Initialize the ROS system and become a node .
+
+  // Initialize the ROS system and become a node.
   ros::init(argc ,argv ,"assignement_b_node");
   ros::NodeHandle nh;
-  // Create a publisher object .
-  ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
-  // Seed the random number generator .
-  srand(time(0));
-  // Loop at 2Hz until the node is shut down .
-  double i = 0.2;
-  ros::Rate rate(2);
-  while (ros::ok()){
-  // Create and fill in the message. The other four
-  // fields, which are ignored by turt lesim , defaultto 0.
-  ros::spinOnce();
-  geometry_msgs::Twist msg;
 
-  double(rand())/double(RAND_MAX);
+  // Subscribe to laser scan output data.
+  ros::Subscriber sub_laser = nh.subscribe("/scan", 1, laserCallback);
 
-  msg.linear.x = 10000;
-  msg.angular.z = 100;
-  i = i + 0.1;
-  // Pub lish the message .
-  pub.publish(msg);
-  // Send a message to rosout with the d e t a i l s .
-  ROS_INFO_STREAM( " Sending ␣random␣velocity␣command : " << "␣linear=" << msg.linear.x << "␣angular=" << msg.angular.z);
-  // Wait un t i l i t ' s time for another i t e ra t ion .
-  rate.sleep();
-  }
+  // Create a publisher object.
+  pub = nh.advertise<Twist>("/cmd_vel", 1000);
+
+  ros::spin();
 }
