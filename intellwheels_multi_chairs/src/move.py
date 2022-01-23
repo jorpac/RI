ca4854c96ae4ab192c4472fed861c02828aa094e
Jorge <up201705754@fe.up.pt>
2022-01-23 13:26:17 +0000

    minor update
    Co-authored-by: Skaarfungandr <Skaarfungandr@users.noreply.github.com>

--- intellwheels_multi_chairs/src/move.py
+++ intellwheels_multi_chairs/src/move.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
+from distutils import cmd
 import math
 import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Twist
 
+global msg
 class LeadingChair(object):
     def __init__(self, pub):
         self.pub = pub
@@ -26,22 +28,30 @@ class FollowerChair(object):
         # alter according to how best move the chair next to follow the one in front/to the side based on scanMsg
         self.pub.publish(newMoveMsg)
 
+# def print(twistMsg):
+#     rospy.loginfo()
+
 def main():
     rospy.init_node('move_chairs')
     #leadPublisher = rospy.Publisher("/robot1/move_base_simple/goal", PoseStamped, queue_size=10)
-    pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=10)
+    pub = rospy.Publisher("/robot2/cmd_vel/", Twist, queue_size=10)
 
     # c1 = LeadingChair(leadPublisher)
     # c2 = FollowerChair(followPublisher)
 
-    # rospy.Subscriber("/robot2/right_front_rplidar_scan", LaserScan, c2.follow)
+    # rospy.Subscriber("/robot2/cmd_vel", print)
     rospy.spin()
 
     cmdMsg = Twist()
-    cmdMsg.linear.x = 1
-
+    cmdMsg.linear.x = 10
     pub.publish(cmdMsg)
+    # Save current time and set publish rate at 10 Hz
+    rate = rospy.Rate(1)
 
+    # For the next 6 seconds publish cmd_vel move commands to Turtlesim
+    while not rospy.is_shutdown():
+        pub.publish(cmdMsg)
+        rate.sleep() 
     # c1.goToGoal((1, 2))
 
 if __name__ == '__main__':
