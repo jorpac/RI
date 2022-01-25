c5f37900192226ae0b06bf6e9da3f5e993972262
Jorge <up201705754@fe.up.pt>
2022-01-25 11:48:07 +0000

    introduced reactive chair

--- intellwheels_multi_chairs/src/move_chairs.py
+++ intellwheels_multi_chairs/src/move_chairs.py
@@ -23,6 +23,23 @@ class FollowerChair(object):
     def follow(self, scanMsg):
         rospy.loginfo('{} \n was the msg received'.format(scanMsg))
         newMoveMsg = Twist()
+        comp_distance = 999999999
+        min_range_index = 0
+        i = 0
+        for distance in  scanMsg.ranges:
+            if (distance < comp_distance):
+                min_range_index = i
+                comp_distance = distance
+            i=i+1
+
+        newMoveMsg.linear.x = 0.4
+        newMoveMsg.angular.z = 0
+        angle = (scanMsg.angle_min * (180 / math.pi)) + (scanMsg.angle_increment * (180 / math.pi)) * min_range_index
+        alpha = 90.0 - abs(angle)
+
+        if (comp_distance <= scanMsg.range_max):
+            newMoveMsg.angular.z = (-40 * (math.sin(alpha * (math.pi / 180)) - (comp_distance - 0.4))) * 0.1
+
         # alter according to how best move the chair next to follow the one in front/to the side based on scanMsg
         self.pub.publish(newMoveMsg)
 
